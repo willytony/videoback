1bf8f168029d45df1bc250bf89fe34ad66f80f67
tony <vemupawankumar2.0@gmail.com>
2023-05-08 20:32:19 +0530

    first commit

---
+++ src/context/SocketProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  const socket = useContext(SocketContext);
+  return socket;
+};
+
+export const SocketProvider = (props) => {
+  // const socket = useMemo(() => io("localhost:5000"), []);
+  // const socket = useMemo(() => io("https://willytony-api.onrender.com"), []);
+  const socket = useMemo(() => io("https://videoapi-p1gw.onrender.com"), []);
+
+  https: return (
+    <SocketContext.Provider value={socket}>
+      {props.children}
+    </SocketContext.Provider>
+  );
+};
